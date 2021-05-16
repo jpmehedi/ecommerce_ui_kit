000128ca500e5f81aec712cd4479495f095ac97f
Mehedi Hasan <mehedie@augnitive.com>
2021-05-16 15:50:50 +0600

    Test published

--- test/ecommerce_ui_kit_test.dart
+++ test/ecommerce_ui_kit_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ecommerce_ui_kit/ecommerce_ui_kit.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// void main() {
+//   test('adds one to input values', () {
+//    // final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
